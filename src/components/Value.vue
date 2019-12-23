35bc6262fbadfe3a77fc029f5d20d40712756a9d
Mitja Karhusaari <mitja@karhusaari.me>
2019-12-23 21:29:04 +0200

    Document the Value component

--- src/components/Value.vue
+++ src/components/Value.vue
@@ -26,12 +26,14 @@
             <th>Binary</th>
             <td>{{ formated('binary') }}</td>
           </tr>
-          <tr v-show="typeof watcher.addresses[computedValue] === 'number'" v-if="depth > 0">
+          <tr
+            v-show="typeof watcher.addresses[computedValue] === 'number'"
+            v-if="depth > 0">
             <th>References</th>
             <td>
               <Value
                 :value="watcher.addresses[computedValue]"
-                :depth="depth-1"/>
+                :depth="depth-1" />
             </td>
           </tr>
         </table>
@@ -42,29 +44,88 @@
 </template>
 
 <script>
+  /**
+   * Component for displaying a numerical value or an address.
+   * Shows an informational tooltip when hovered which contains
+   * the value in decimal, binary and hex as well as the value
+   * of the memory location the value points to.
+   */
   export default {
     name: 'Value',
 
     data () {
       return {
+        /**
+         * @name watcher
+         * @type {MemoryWatcher}
+         */
         watcher: this.$emulator.getWatcher(),
+
+        /**
+         * Identifier of a currently active timer which will trigger the popup with
+         * {@link Value#showPopup}. If the mouse moves off of the component,
+         * {@link Value#onMouseOut} clears the timeout and sets this to null.
+         *
+         * @name Value.hoverTimeout
+         * @type {number?}
+         * @default null
+         */
         hoverTimeout: null,
+
+        /**
+         * Whether or not the popup is currently visible.
+         *
+         * @name popupVisible
+         * @type {boolean}
+         * @default false
+         */
         popupVisible: false,
+
+        /**
+         * Offset of the popup on the x-axis in pixels.
+         * Used to shift the popup so that it is fully visible when near the edges
+         * of the visible area.
+         * Re-calculated whenever {@link Value#showPopup} is called.
+         *
+         * @name offsetX
+         * @type {number!}
+         * @default 0
+         */
         offsetX: 0,
       };
     },
 
     props: {
+      /**
+       * If a pointer has multiple levels of indirection, we can show popups for the
+       * values inside other popups. This property sets the maximum depth of nested
+       * popups. Defaults to 0 for performance reasons.
+       */
       depth: {
         type: Number,
         default: 0,
       },
+
+      /**
+       * This property can be used to set the displayed value explicitly.
+       */
       value: Number,
+
+      /**
+       * Alternative to {@link Value.value} that displays the value of the given
+       * memory location. If the parent component already watches the value of the
+       * address, it is advisable to use {@link Value.value} instead.
+       */
       address: Number,
+
+      /**
+       * Specifies the format (numeral system) in which the in-line value is displayed.
+       * Supports the following values: decimal, binary and hexadecimal.
+       */
       format: {
         type: String,
         default: 'decimal',
-        validator: value => ['decimal', 'binary', 'hexadecimal'].indexOf(value) !== -1,
+        validator: (value) => ['decimal', 'binary', 'hexadecimal'].indexOf(value) !== -1,
       },
     },
 
@@ -73,14 +134,20 @@
     },
 
     watch: {
+      /**
+       * Updates the watched address whenever the {@link Value#value} prop updates.
+       */
       value: {
         immediate: true,
         async handler (newValue, oldValue) {
-          console.log(`value ${oldValue} -> ${newValue}`);
           this.watcher.unwatch(oldValue);
           await this.watcher.watch(newValue);
         },
       },
+
+      /**
+       * Updates the watched addresses whenever the {@link Value#address} prop updates.
+       */
       address: {
         immediate: true,
         async handler (newAddress, oldAddress) {
@@ -94,6 +161,10 @@
           await this.watcher.watch(newAddress);
         },
       },
+
+      /**
+       * Updates the watcher for the referenced value whenever the pointer changes.
+       */
       'watcher.addresses': {
         immediate: true,
         async handler (newAddresses, oldAddresses) {
@@ -112,15 +183,26 @@
     },
 
     computed: {
-      computedValue () {
-        if (typeof this.value === 'number')
+      /**
+       * The displayed value.
+       * If the value is defined by an address, resolves to the value at the location
+       * specified by the {@link Value.address} proprty. Otherwise resolves to the
+       * value of specified with {@link Value.value}.
+       */
+      computedValue() {
+        if (typeof this.value === 'number') {
           return this.value;
+        }
 
         return this.watcher.addresses[this.address];
       },
     },
 
     methods: {
+      /**
+       * Returns the value ({@link Value.computedValue}) in the specified
+       * numbering system.
+       */
       formated(format) {
         if (typeof this.computedValue !== 'number') {
           return 'Undef.';
@@ -136,7 +218,7 @@
           let i = str.length;
           while (i > 0) {
             const next = Math.max(0, i - interval);
-            result = str.slice(next, i) + ' ' + result;
+            result = `${str.slice(next, i)} ${result}`;
             i = next;
           }
 
@@ -158,17 +240,39 @@
           const pad = '0'.repeat(32 - num.length);
           return `${sign}${insertSpaces(pad + num, 8)}`;
         }
+
+        return 'Invalid format';
       },
 
-      onMouseOver () {
+      /**
+       * Starts the timer for displaying the popoup whenever the cursor
+       * enters the component.
+       *
+       * @listens MouseOverEvent
+       */
+      onMouseOver() {
         this.hoverTimeout = setTimeout(this.showPopup.bind(this), 200);
       },
 
-      onMouseOut () {
+      /**
+       * Clears the timer for displaying the popup whenever the cursor
+       * leaves the component. Hiding the popup is handled by a global event
+       * listener registered in {@link Value#showPopup}.
+       *
+       * @listens MouseOutEvent
+       */
+      onMouseOut() {
         clearTimeout(this.hoverTimeout);
       },
 
-      showPopup () {
+      /**
+       * Calculates the {@link Value.offsetX} value for the popup based on it's
+       * location relative to the panel's visible area and makes the popup visible.
+       *
+       * Registers a global callback for `mousemove` event which hides the popup
+       * when the mouse moves outside of the popup.
+       */
+      showPopup() {
         this.popupVisible = true;
 
         const popupRect = this.$refs.popup.getBoundingClientRect();
